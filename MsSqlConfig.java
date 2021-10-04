4706f88a8a123e33c20037b5e4ea172bb3ebc626
Joao Lambert <54284662+devlambertjoao@users.noreply.github.com>
2021-10-04 18:13:59 -0300

    Update MsSqlConfig.java

--- MsSqlConfig.java
+++ MsSqlConfig.java
@@ -2,8 +2,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EntityScan("com.lambert.product.data.mssql.entity")
-@EnableJpaRepositories("com.lambert.product.data.mssql.repository")
+@EntityScan("com.xxx.entity")
+@EnableJpaRepositories("com.xxx.repository")
 @Configuration
 public class MsSqlConfig {
 
